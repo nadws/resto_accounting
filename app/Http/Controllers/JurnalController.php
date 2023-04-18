3a04723e1d5beb70b68d4fc223950dd970ba5670
aldmf26 <aldimf26@gmail.com>
2023-04-18 14:53:02 +0800

    1213

--- app/Http/Controllers/JurnalController.php
+++ app/Http/Controllers/JurnalController.php
@@ -43,7 +43,7 @@ class JurnalController extends Controller
             'id_proyek' => $id_proyek
 
         ];
-        return view('Jurnal.index', $data);
+        return view('jurnal.index', $data);
     }
 
     public function add()
@@ -61,7 +61,7 @@ class JurnalController extends Controller
             'proyek' => proyek::all()
 
         ];
-        return view('Jurnal.add', $data);
+        return view('jurnal.add', $data);
     }
 
     public function load_menu()
@@ -72,7 +72,7 @@ class JurnalController extends Controller
             'proyek' => proyek::all()
 
         ];
-        return view('Jurnal.load_menu', $data);
+        return view('jurnal.load_menu', $data);
     }
     public function tambah_baris_jurnal(Request $r)
     {
@@ -82,7 +82,7 @@ class JurnalController extends Controller
             'count' => $r->count
 
         ];
-        return view('Jurnal.tbh_baris', $data);
+        return view('jurnal.tbh_baris', $data);
     }
 
     public function save_jurnal(Request $r)
@@ -173,7 +173,7 @@ class JurnalController extends Controller
             'head_jurnal' => DB::selectOne("SELECT a.tgl, a.id_proyek, a.no_dokumen,a.tgl_dokumen, sum(a.debit) as debit , sum(a.kredit) as kredit FROM jurnal as a where a.no_nota = '$r->no_nota'")
 
         ];
-        return view('Jurnal.edit', $data);
+        return view('jurnal.edit', $data);
     }
 
     public function edit_save(Request $r)
@@ -222,6 +222,6 @@ class JurnalController extends Controller
             where a.no_nota = '$r->no_nota'")
 
         ];
-        return view('Jurnal.detail', $data);
+        return view('jurnal.detail', $data);
     }
 }
