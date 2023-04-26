5920ef5e2ccb88ec4c1afaddf4ff761ee67ffb63
nadws <nandw567@gmail.com>
2023-04-26 13:42:55 +0800

    da

---
+++ database/migrations/2023_04_26_053144_create_kelompok_aktiva_table.php.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelompok_aktiva', function (Blueprint $table) {
+            $table->integerIncrements('id_kelompok');
+            $table->string('nm_kelompok');
+            $table->string('umur');
+            $table->double('tarif');
+            $table->string('barang_kelompok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
