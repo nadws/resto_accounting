b4b011c32921dce79226f08d6f56215a9c25596b
aldmf26 <aldimf26@gmail.com>
2023-07-19 13:38:33 +0800

    213

--- app/Http/Controllers/BukuBesarController.php
+++ app/Http/Controllers/BukuBesarController.php
@@ -90,7 +90,7 @@ class BukuBesarController extends Controller
                             WHERE j.id_akun != '$r->id_akun'
                             GROUP BY j.no_nota
                         ) d ON a.no_nota = d.no_nota AND d.id_akun != a.id_akun
-                        WHERE a.id_akun = '$r->id_akun' and a.tgl between '$tgl1' and '$tgl2' AND a.penutup = 'T'
+                        WHERE a.id_akun = '$r->id_akun' and a.tgl between '$tgl1' and '$tgl2'
                         order by a.saldo DESC, a.tgl ASC
             "),
             'id_akun' => $r->id_akun,
