36a085f64ff8bad225487fd680ccd6e92525623d
aldmf26 <aldimf26@gmail.com>
2023-07-19 13:41:34 +0800

    213

--- app/Http/Controllers/BukuBesarController.php
+++ app/Http/Controllers/BukuBesarController.php
@@ -59,7 +59,7 @@ class BukuBesarController extends Controller
         $buku = DB::select("SELECT a.no_nota,a.id_akun, b.kode_akun, b.nm_akun, sum(a.debit) as debit , sum(a.kredit) as kredit 
         FROM jurnal as a 
         left join akun as b on b.id_akun = a.id_akun
-        WHERE a.tgl BETWEEN '$tgl1' and '$tgl2' AND a.penutup = 'T'
+        WHERE a.tgl BETWEEN '$tgl1' and '$tgl2'
         group by a.id_akun
         ORDER by b.kode_akun ASC;");
 
