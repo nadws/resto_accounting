393ff29ee346f5cea39012364843a05078cf4894
aldmf26 <aldimf26@gmail.com>
2023-07-21 08:49:06 +0800

    213

--- app/Imports/JurnalImport.php
+++ app/Imports/JurnalImport.php
@@ -21,18 +21,10 @@ class JurnalImport implements ToModel, WithHeadingRow
                 'no_nota' => $row['no_nota'],
                 'ket' => $row['ket'],
                 'no_dokumen' => $row['no_dokumen'],
-                'tgl_dokumen' => $row['tgl_dokumen'],
                 'debit' => $row['debit'],
                 'kredit' => $row['kredit'],
                 'admin' => $row['admin'],
-                'id_proyek' => $row['id_proyek'],
-                'id_departemen' => $row['id_departemen'],
                 'id_post_center' => $row['id_post_center'],
-                'no_urut' => $row['no_urut'],
-                'urutan' => $row['urutan'],
-                'saldo' => $row['saldo'],
-                'penutup' => $row['penutup'],
-                'kode_penyesuaian' => $row['kode_penyesuaian'],
                 'setor' => $row['setor'],
             ]
         );
