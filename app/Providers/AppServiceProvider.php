fbb0691f4d624a3c5891bb73a5c6686656df2ba5
aldmf26 <aldimf26@gmail.com>
2023-12-21 13:04:00 +0800

    32

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
 
         $logoTkmr = asset('assets/login/img/takemori_3.jpg');
         $logoSdb = asset('assets/login/img/sdb_logo.png');
-
+        dd(request()->getHost() === $link2);
         $gambarLogo = request()->getHost() === $link2 ? $logoSdb : $logoTkmr;
 
         app()->singleton('link1', function () use ($link1) {
