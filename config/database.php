93e1360da195bc61e998e7d3f92b9d0f5a345390
aldmf26 <aldimf26@gmail.com>
2024-02-08 13:10:01 +0800

    dsa

--- config/database.php
+++ config/database.php
@@ -93,7 +93,7 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-    ],
+    ],  
 
     /*
     |--------------------------------------------------------------------------
